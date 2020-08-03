e11ec4c3f8e3fcc6af8fceb7dd282aa572c9490b
AlbertXiao <albert.xiao@grapecity.com>
2020-08-03 10:55:01 +0800

    ⚡️ update tsconfig types and rm test types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as constants from "@src/constants/index";
 import * as requestFun from "@src/request/fun";
 import * as hexConvert from "@src/hexConvert/index";
+import * as _ from "@src/_/index";
 
-export { constants, requestFun, hexConvert };
+export { constants, requestFun, hexConvert, _ };
